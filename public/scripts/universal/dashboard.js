04aa454934429d7d491b95afea7099a853dc64a8
mrhardy23 <collinsadi20@gmail.com>
2023-08-27 07:55:29 +0100

    worked on active class for mobile footer menu

--- public/scripts/universal/dashboard.js
+++ public/scripts/universal/dashboard.js
@@ -1,1 +1,13 @@
 // alert(url)
+
+const mobileButtons = document.querySelectorAll(".mobile-side-bar-button")
+
+mobileButtons.forEach(button => {
+    
+    if (button.classList.contains("active-mobile")) {
+        
+        const span = button.querySelector("span")
+
+        span.style.display = "inline-block"
+    }
+})
