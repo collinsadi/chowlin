b789ac45cce5e359be0d7ce7a2fec8ba023ac967
mrhardy23 <collinsadi20@gmail.com>
2023-09-26 02:42:21 +0100

    cookies

--- public/scripts/config/url.js
+++ public/scripts/config/url.js
@@ -1,11 +1,45 @@
 
-const cookie = document.cookie.split("=")[1]
+const getCookie = (cookieName) => {
+
+    const cookies = document.cookie.split("; ");
+    
+    for (const cookie of cookies) {
+        const [name, value] = cookie.split("=");
+        if (name === cookieName) {
+            
+            return decodeURIComponent(value);
+        }
+    }
+    
+    return null
+
+}
+
+// const getCookie = (cookieName) => {
+
+//     const cookies = document.cookie.split(";");
+    
+//     cookies.forEach(cookie => {
+//         const name = cookie.split("=")[0]
+//         if (cookieName === name) {
+            
+//             return cookie.split("=")[1]
+//         } else {
+            
+//             return null
+//         }
+//     })
+
+// }
+
+
+
+// const cookie = document.cookie.split("=")[1]
+const cookie = getCookie("jwt")
 // const url = "https://chowlinapi.onrender.com"
 const url = "http://localhost:5000"
 
-const vendorCookie = document.cookie.split("=")[1]
-
-
+const vendorCookie = getCookie("vend")
 
 const setCookie = (name,value,days) => {
     
